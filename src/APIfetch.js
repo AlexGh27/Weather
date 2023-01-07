ebe366090ead17cb48f83cfe240ef03c16cf0dbf
AlexGh27 <alexghioca27@gmail.com>
2023-01-07 13:41:33 +0200

    Added additional info

--- src/APIfetch.js
+++ src/APIfetch.js
@@ -1,19 +1,34 @@
 function defaultWeather(response) {
     let cityName = document.querySelector('#cityName');
     cityName.textContent = response.name;
+
+    let degrees = document.querySelector('#degrees');
+    degrees.textContent = Math.round(response.main.temp) + '°C';
+
+    let feels = document.querySelector('#feels'); 
+    feels.textContent = 'Feels like ' + Math.round(response.main.feels_like) + '°C';
+
+    let humidity = document.querySelector('#humidity');
+    humidity.textContent = 'Humidity: ' + response.main.humidity + '%';
+
+    let wind = document.querySelector('#wind');
+    wind.textContent = 'Wind: ' + Math.round(response.wind.speed) + ' km/h';
+
+    let unixTime = response.dt;
+    console.log(unixTime);
 }
 
 
 
 function fetchWeather() {
     const API_KEY = 'ac4b3a21bc90e7e4ae8ef6cb4b06fc97';
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=Bucharest&appid=${API_KEY}`, {mode: 'cors'})
+    const city = 'Galati';
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`, {mode: 'cors'})
         .then(function(response) {
             return response.json();
         })
         .then(function(response) {
             defaultWeather(response);
-            
         })
 }
 

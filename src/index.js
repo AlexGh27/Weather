ae160eb051ce239cbf38a5c68b52e4d7d3052a1a
AlexGh27 <alexghioca27@gmail.com>
2023-01-10 10:48:26 +0200

    Error while searchbar is empty

--- src/index.js
+++ src/index.js
@@ -4,13 +4,6 @@ import {defaultWeather, displayWeather, searchWeather} from './APIfetch.js';
 const searchButton = document.querySelector('#searchButton');
 const city = document.querySelector('#searchBar').value;
 
-searchButton.addEventListener('click', function(e) {
-    if (city === '') {
-        alert('Please enter a city name!')
-    }
-    else {
-        searchWeather();
-    }
-})
+searchButton.addEventListener('click', searchWeather)
 
 defaultWeather();
